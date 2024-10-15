177318d522e75d18f04cf3073fab1266fa24ba2a
rod773 <rodrigoarcadio1@gmail.com>
2024-10-15 17:46:19 +0200

    inital

---
+++ src/App.js
@@ -0,0 +1,46 @@
+import { useState } from "react";
+import { AgGridReact } from "ag-grid-react"; // React Data Grid Component
+import "ag-grid-community/styles/ag-grid.css"; // Mandatory CSS required by the Data Grid
+import "ag-grid-community/styles/ag-theme-quartz.css"; // Optional Theme applied to the Data Grid
+
+function App() {
+  const [rowData, setRowData] = useState([
+    { make: "Tesla", model: "Model Y", price: 64950, electric: true },
+    { make: "Ford", model: "F-Series", price: 33850, electric: false },
+    { make: "Toyota", model: "Corolla", price: 29600, electric: false },
+    { make: "Mercedes", model: "EQA", price: 48890, electric: true },
+    { make: "Fiat", model: "500", price: 15774, electric: false },
+    { make: "Nissan", model: "Juke", price: 20675, electric: false },
+  ]);
+
+  // Column Definitions: Defines & controls grid columns.
+  const [colDefs, setColDefs] = useState([
+    { field: "make" },
+    { field: "model" },
+    { field: "price" },
+    { field: "electric" },
+  ]);
+
+  const defaultColDef = {
+    flex: 1,
+  };
+  return (
+    <>
+      <div
+        className="ag-theme-quartz" // applying the Data Grid theme
+        style={{
+          height: "100%",
+          width: "100%",
+        }} // the Data Grid will fill the size of the parent container
+      >
+        <AgGridReact
+          rowData={rowData}
+          columnDefs={colDefs}
+          defaultColDef={defaultColDef}
+        />
+      </div>
+    </>
+  );
+}
+
+export default App;
